7011ac69aedc99626b373d1e16370344f9e141ac
sibendra07 <sibendra07@gmail.com>
2024-07-02 10:34:56 +0545

    Initial commit

---
+++ Sibtweets/tweet/forms.py
@@ -0,0 +1,17 @@
+from django import forms 
+from .models import Tweet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+class TweetForm(forms.ModelForm):  
+    class Meta:
+        model = Tweet
+        fields = ['text','photo']
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ('username','email','password1','password2')
+
+class TweetSearchForm(forms.Form):
+    text = forms.CharField(max_length=280, required=False, label='Search Tweets')
